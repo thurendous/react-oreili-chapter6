d327235a2cc5def1a8552638172ff13ae784ba5d
thurendous <thurendous2@gmail.com>
2021-11-17 13:22:18 +0900

    add component of form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import colorData from "./color-data";
 import ColorList from "./ColorList";
+import AddColorForm from "./addColorForm";
 
 export default function App() {
   const [colors, setColors] = useState(colorData);
   return (
-    <ColorList
-      colors={colors}
-      onRemoveC={(id) => {
-        const newColors = colors.filter((color) => color.id !== id);
-        setColors(newColors);
-      }}
-      onRateColor={(id, rating) => {
-        const newColors = colors.map((color) =>
-          color.id === id ? { ...color, rating } : color
-        );
-        setColors(newColors);
-      }}
-    />
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => alert(`TODO: Create ${title} - ${color}`)}
+      />
+      <ColorList
+        colors={colors}
+        onRemoveC={(id) => {
+          const newColors = colors.filter((color) => color.id !== id);
+          setColors(newColors);
+        }}
+        onRateColor={(id, rating) => {
+          const newColors = colors.map((color) =>
+            color.id === id ? { ...color, rating } : color
+          );
+          setColors(newColors);
+        }}
+      />
+    </>
   );
 }
